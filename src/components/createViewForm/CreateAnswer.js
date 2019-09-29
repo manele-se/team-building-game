fc92e441eb8c54a7e834065b6169cf77eeb52a24
Elena <elena@manele.se>
2019-09-29 17:23:45 +0200

    answers in database and clear

--- src/components/createViewForm/CreateAnswer.js
+++ src/components/createViewForm/CreateAnswer.js
@@ -1,33 +1,42 @@
-import React, { Component } from "react";
-import "./create-answer.css";
+import React, { Component } from 'react';
+import './create-answer.css';
+import { threadId } from 'worker_threads';
 
 class CreateAnswer extends Component {
-  text = "";
-  correct = false;
+	text = '';
+	correct = false;
 
-  onTextChanged = e => {
-    this.text = e.target.value;
-  };
+	onTextChanged = (e) => {
+		this.text = e.target.value;
+	};
 
-  onCheckboxChanged = e => {
-    this.correct = e.target.checked;
-  };
+	onCheckboxChanged = (e) => {
+		this.correct = e.target.checked;
+	};
 
-  //need to be a consumer to save the correct answer?
-  render() {
-    return (
-      <div className="create-answer">
-        <input
-          type="text"
-          name="answer"
-          onInput={e => this.onTextChanged(e)}
-          autoComplete="off"
-          placeholder="Type an answer..."
-        />
-        <input onInput={e => this.onCheckboxChanged(e)} type="checkbox" />
-      </div>
-    );
-  }
+	refAnswer = React.createRef();
+	refCorrect = React.createRef();
+
+	clearAnswer = () => {
+		this.refAnswer.current.value = '';
+		this.refCorrect.current.checked = false;
+	};
+	//need to be a consumer to save the correct answer?
+	render() {
+		return (
+			<div className="create-answer">
+				<input
+					type="text"
+					name="answer"
+					onInput={(e) => this.onTextChanged(e)}
+					autoComplete="off"
+					placeholder="Type an answer..."
+					ref={this.refAnswer}
+				/>
+				<input onInput={(e) => this.onCheckboxChanged(e)} type="checkbox" ref={this.refCorrect} />
+			</div>
+		);
+	}
 }
 
 export default CreateAnswer;
--- src/components/createViewForm/createPlayerForm.js
+++ src/components/createViewForm/createPlayerForm.js
@@ -1,86 +1,87 @@
-import React from "react";
-import { Consumer } from "../../context";
-import Header from "../../layouts/Header";
-import AddQuestionButton from "../../layouts/buttons/AddQuestionButton";
-import CreateAnswer from "./CreateAnswer";
-import Spinner from "../../layouts/Spinner";
-import CreateQuestion from "./CreateQuestion";
+import React from 'react';
+import { Consumer } from '../../context';
+import Header from '../../layouts/Header';
+import AddQuestionButton from '../../layouts/buttons/AddQuestionButton';
+import CreateAnswer from './CreateAnswer';
+import Spinner from '../../layouts/Spinner';
+import CreateQuestion from './CreateQuestion';
 
 class CreatePlayerForm extends React.Component {
-  // Reference to the question title input element
-  questionRef = React.createRef();
+	// Reference to the question title input element
+	questionRef = React.createRef();
 
-  // TODO: Refactor to make more generic
-  answers = [
-    {
-      ref: React.createRef()
-    },
-    {
-      ref: React.createRef()
-    },
-    {
-      ref: React.createRef()
-    }
-  ];
+	// TODO: Refactor to make more generic
+	answers = [
+		{
+			ref: React.createRef()
+		},
+		{
+			ref: React.createRef()
+		},
+		{
+			ref: React.createRef()
+		}
+	];
 
-  onAddQuestionCreated = dispatch => {
-    const question = this.questionRef.current.value;
+	onAddQuestionCreated = (dispatch) => {
+		const question = this.questionRef.current.value;
 
-    console.log(`New question: ${question}`);
+		console.log(`New question: ${question}`);
 
-    dispatch({
-      type: "ADD_QUESTION",
-      payload: {
-        title: question,
-        answers: this.answers.map(answer => ({
-          text: answer.ref.current.text,
-          isCorrect: answer.ref.current.correct
-        }))
-      }
-    });
+		dispatch({
+			type: 'ADD_QUESTION',
+			payload: {
+				title: question,
+				answers: this.answers.map((answer) => ({
+					text: answer.ref.current.text,
+					isCorrect: answer.ref.current.correct
+				}))
+			}
+		});
 
-    this.questionRef.current.value = "";
-  };
-  render() {
-    return (
-      <Consumer>
-        {value => {
-          const { playerId } = this.props.match.params;
-          const { dispatch, player } = value;
-          console.log({ player });
-          if (player && player.id) {
-            return (
-              <React.Fragment>
-                <Header />
-                <div className="container customCreateQuestionsContainer">
-                  <form>
-                    <CreateQuestion textRef={this.questionRef} />
-                    <div className="container  answerContainer">
-                      {this.answers.map((answer, index) => (
-                        <CreateAnswer key={index} ref={answer.ref} />
-                      ))}
-                      <AddQuestionButton
-                        onClick={() => {
-                          this.onAddQuestionCreated(dispatch);
-                        }}
-                      />
-                    </div>
-                  </form>
-                </div>
-              </React.Fragment>
-            );
-          } else {
-            value.subscribePlayer(playerId);
-            return (
-              <div>
-                <Spinner />
-              </div>
-            );
-          }
-        }}
-      </Consumer>
-    );
-  }
+		this.questionRef.current.value = '';
+		this.answers.forEach((answer) => answer.ref.current.clearAnswer());
+	};
+	render() {
+		return (
+			<Consumer>
+				{(value) => {
+					const { playerId } = this.props.match.params;
+					const { dispatch, player } = value;
+					console.log({ player });
+					if (player && player.id) {
+						return (
+							<React.Fragment>
+								<Header />
+								<div className="container customCreateQuestionsContainer">
+									<form>
+										<CreateQuestion textRef={this.questionRef} />
+										<div className="container  answerContainer">
+											{this.answers.map((answer, index) => (
+												<CreateAnswer key={index} ref={answer.ref} />
+											))}
+											<AddQuestionButton
+												onClick={() => {
+													this.onAddQuestionCreated(dispatch);
+												}}
+											/>
+										</div>
+									</form>
+								</div>
+							</React.Fragment>
+						);
+					} else {
+						value.subscribePlayer(playerId);
+						return (
+							<div>
+								<Spinner />
+							</div>
+						);
+					}
+				}}
+			</Consumer>
+		);
+	}
 }
 
 export default CreatePlayerForm;
