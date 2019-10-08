2c05e16551c83b1e68bc1829446e6bc74dfd20ca
Elena <elena@manele.se>
2019-10-08 18:17:39 +0200

    removed play link from scoreboard

--- src/components/scoreView/PlayerProgressBar.js
+++ src/components/scoreView/PlayerProgressBar.js
@@ -1,43 +1,32 @@
-import React, { Component } from "react";
-import ProgressBar from "react-bootstrap/ProgressBar";
-import avatars from "../../images";
+import React, { Component } from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import avatars from '../../images';
 
 //show progress for each player
 
 class PlayerProgressBar extends Component {
-  render() {
-    //consumer av states
-    //check the score for a player
-    const { playerId, name, score, avatar } = this.props;
-    return (
-      <div className="player-progress-bar">
-        <ProgressBar
-          striped
-          variant="success"
-          now={score}
-          className="progress"
-        />
-        <p>
-          <img
-            src={avatars[avatar]}
-            className="avatar"
-            alt="avatar"
-            style={{
-              marginLeft: `${score * 0.95}%`,
-              transition: "margin-left 0.5s ease-out"
-            }}
-          />{" "}
-        </p>
-        <p>
-          {name}{" "}
-          <em>
-            (player link:{" "}
-            <a href={`/play/${playerId}`}>{`/play/${playerId}`}</a>
-          </em>
-        </p>
-      </div>
-    );
-  }
+	render() {
+		//consumer av states
+		//check the score for a player
+		const { playerId, name, score, avatar } = this.props;
+		return (
+			<div className="player-progress-bar">
+				<ProgressBar striped variant="success" now={score} className="progress" />
+				<p>
+					<img
+						src={avatars[avatar]}
+						className="avatar"
+						alt="avatar"
+						style={{
+							marginLeft: `${score * 0.95}%`,
+							transition: 'margin-left 0.5s ease-out'
+						}}
+					/>{' '}
+				</p>
+				<p>{name}</p>
+			</div>
+		);
+	}
 }
 
 export default PlayerProgressBar;
