849091c159044833e474feac42308445d4571e84
Elena Marzi <elena.marzi@acando.com>
2019-10-09 11:21:14 +0200

    small changes in the layout

--- src/components/scoreView/PlayerProgressBar.js
+++ src/components/scoreView/PlayerProgressBar.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import avatars from '../../images';
+import React, { Component } from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import avatars from "../../images";
 
 //show progress for each player
+//do not show avatar if anwers is about the same avatar!!!
+//do not show the name of the avatar, just the picture
 
 class PlayerProgressBar extends Component {
-	render() {
-		//consumer av states
-		//check the score for a player
-		const { name, score, avatar, currentSubject } = this.props;
-		const className = currentSubject ? 'player-progress-bar current-player' : 'player-progress-bar';
-		return (
-			<div className={className}>
-				<ProgressBar striped variant="success" now={score} />
-				<div className="player-avatar-holder">
-					<img
-						src={avatars[avatar]}
-						className="player-avatar"
-						alt="avatar"
-						style={{
-							marginLeft: `calc(${Math.round(score)}% - 3rem)`
-						}}
-					/>{' '}
-				</div>
-				<div className="player-name">{name}</div>
-			</div>
-		);
-	}
+  render() {
+    const { score, avatar, currentSubject } = this.props;
+    const className = currentSubject
+      ? "player-progress-bar current-player"
+      : "player-progress-bar";
+    return (
+      <div className={className}>
+        <ProgressBar striped variant="success" now={score} />
+        <div className="player-avatar-holder">
+          <img
+            src={avatars[avatar]}
+            className="player-avatar"
+            alt="avatar"
+            style={{
+              marginLeft: `calc(${Math.round(score)}% - 3rem)`
+            }}
+          />{" "}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default PlayerProgressBar;
--- src/components/scoreView/ScoreView.js
+++ src/components/scoreView/ScoreView.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import soundfile from '../../music/music-remixed.mp3';
-import ScoreBoard from './ScoreBoard';
+import React, { Component } from "react";
+import soundfile from "../../music/music.mp3";
+import ScoreBoard from "./ScoreBoard";
 
+//music B
 class ScoreView extends Component {
-	render() {
-		const { gameId } = this.props;
-		return (
-			<React.Fragment>
-				<div className="scoreViewBackgound">
-					<ScoreBoard gameId={gameId} />
-					<audio src={soundfile} autoPlay loop />
-				</div>
-			</React.Fragment>
-		);
-	}
+  render() {
+    const { gameId } = this.props;
+    return (
+      <React.Fragment>
+        <div className="scoreViewBackgound">
+          <ScoreBoard gameId={gameId} />
+          <audio src={soundfile} autoPlay loop />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default ScoreView;
