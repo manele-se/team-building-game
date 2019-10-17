c1eb54bc4479688d7579f167f1f6e76dfc81ecab
Elena Marzi <elena.marzi@acando.com>
2019-10-17 22:11:47 +0200

    Winner View

--- src/components/scoreView/WinnerView.js
+++ src/components/scoreView/WinnerView.js
@@ -1,14 +1,48 @@
 import React, { Component } from "react";
+import { Consumer } from "../../context";
+
+import soundfile from "../../music/music4.mp3";
+import Spinner from "../../layouts/Spinner";
+import avatars from "../../images";
 
 //visar vinnare. den som har svarat mest frågor om ett subject.
 //lägg till nmnet på vinnare
 export default class WinnerView extends Component {
   render() {
+    //matching the parameters in the url
+    const { gameId } = this.props.match.params;
+
+    // TODO: find out who won!
+    // For now, just use the first player!
+
     return (
-      <div classNanem="container">
-        <h2>Grattis vinnare, du är subjektets bästa vän</h2>
-        <button>Nästa människa eller game over</button>
-      </div>
+      <Consumer>
+        {value => {
+          const { game } = value;
+          if (game && game.id) {
+            const winner = game.players[0];
+
+            return (
+              <div className="scoreViewBackgound">
+                <div className="container">
+                  <div className="container choosenAvatar ">
+                    <img src={avatars[winner.avatar]} alt="avatar" />
+                  </div>
+                  <h1 className="round">You win!</h1>
+                  <audio src={soundfile} autoPlay loop />
+                </div>
+              </div>
+            );
+          } else {
+            value.subscribe(gameId);
+            return (
+              <div>
+                <Spinner />
+              </div>
+            );
+          }
+        }}
+      </Consumer>
     );
   }
 }
