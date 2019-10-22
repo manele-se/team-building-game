20a3d1d026262362816c0f318a1c8e0ba14077e1
Elena <elena@manele.se>
2019-10-22 19:50:11 +0200

    Moving forward with game engine!

--- src/GameEngine.js
+++ src/GameEngine.js
@@ -1,41 +1,54 @@
-import React from "react";
-import { Consumer } from "./context";
+import React from 'react';
+import { Consumer } from './context';
 
-const delay = ms =>
-  new Promise((resolve, reject) => window.setTimeout(resolve, ms));
+const delay = (ms) => new Promise((resolve, reject) => window.setTimeout(resolve, ms));
 
-const NONE = "NONE";
-const WAITING = "WAITING";
+const same = (one, other) => {
+  if (one === other) return true;
+  return JSON.stringify(one) === JSON.stringify(other);
+};
+
+const NONE = 'NONE';
+const WAITING = 'WAITING';
+const READY_TO_PLAY = 'READY_TO_PLAY';
+const SHOW_CURRENT_SUBJECT = 'SHOW_CURRENT_SUBJECT';
+const SHOW_CURRENT_QUESTION = 'SHOW_CURRENT_QUESTION';
 
 class GameEngine extends React.Component {
   state = {
     gameId: null,
     game: null,
-    players: null,
+    players: [],
     currentSubjectIndex: -1,
     currentSubject: null,
     currentQuestionIndex: -1,
-    currentQuestion: null
+    currentQuestion: null,
+    gameState: NONE
   };
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.gameUpdateCallbacks = [];
+    this.playerUpdateCallbacks = [];
   }
 
   componentDidMount() {
-    const { value } = this.props;
+    const { value, gameId } = this.props;
 
     this.dispatch = value.dispatch;
     this.subscribeDoc = value.subscribeDoc;
-    this.gameState = NONE;
+    this.updateDoc = value.updateDoc;
 
-    this.playGame();
+    this.playGame(gameId);
   }
 
-  async playGame() {
-    this.setState({ gameState: WAITING });
+  async playGame(gameId) {
+    this.setState({ gameState: WAITING, gameId: gameId });
     await this.startSubscriptions();
     await this.gameIsReadyToPlay();
+
+    this.setState({ gameState: READY_TO_PLAY });
+    await this.startButtonIsClicked();
 
     // Loop as many times as there are players
     for (let i = 0; i < this.state.players.length; i++) {
@@ -43,85 +56,217 @@ class GameEngine extends React.Component {
       await this.showCurrentSubject();
 
       // Loop as many times are this subject has questions
-      for (let j = 0; j < this.subject.questions.length; j++) {
-        this.pickNextQuestion();
-        this.showCurrentQuestion();
+      for (let j = 0; j < this.state.currentSubject.questions.length; j++) {
+        await this.showCurrentQuestion(j);
         await this.allPlayersHaveAnswered();
+        await delay(3000);
         await this.showCorrectAnswers();
+        await delay(5000);
       }
 
       if (this.state.players.length >= 3) {
         await this.showWinnerForThisSubject();
+        await delay(10000);
       }
     }
 
-    await this.showTotalWinner();
-  }
-
-  render() {
-    const { children } = this.props;
-
-    const childrenWithGameEngine = React.Children.toArray(children)
-      .filter(child => {
-        const { gameState } = child.props;
-        console.log(`Comparing "${gameState}" to ${this.state.gameState}"`);
-        return gameState === this.state.gameState;
-      })
-      .map(child => {
-        return React.cloneElement(child, { gameEngine: this });
-      });
-    return <React.Fragment>{childrenWithGameEngine}</React.Fragment>;
+    //await this.showTotalWinner();
   }
 
   // This method starts all Firestore subscriptions
   async startSubscriptions() {
-    this.gameUpdateCallbacks = [];
-    this.subscribeDoc("games", this.props.gameId, gameDoc =>
-      this.receiveGameDoc(gameDoc)
-    );
-    const gameDoc = await this.gameIsUpdated(true, true);
-    console.log(`Game is loaded!`, JSON.stringify(gameDoc, null, "  "));
+    // Subscribe to the games/ID doc
+    this.subscribeDoc('games', this.props.gameId, (gameDoc) => this.setState({ game: gameDoc }));
+    const gameDoc = await this.gameIsUpdated(true);
+
+    // Subscribe to all the players/ID docs for this game
+    gameDoc.players.forEach((gamePlayer, index) => {
+      this.subscribeDoc('players', gamePlayer.id, (playerDoc) => {
+        const newPlayers = [ ...this.state.players ];
+        newPlayers[index] = playerDoc;
+        this.setState({
+          players: newPlayers
+        });
+      });
+    });
   }
 
-  async receiveGameDoc(gameDoc) {
-    this.setState({ game: gameDoc });
-    const newGameUpdateCallbacks = [];
-    this.gameUpdateCallbacks.forEach(({ filter, callback, once }) => {
-      const filterResult =
-        typeof filter === "function" ? filter(gameDoc) : filter;
-      if (filterResult) callback(gameDoc);
-      if (!(filterResult && once)) {
-        newGameUpdateCallbacks.push({ filter, callback, once });
-      }
+  // This method waits until all players have created questions, and selected avatars
+  async gameIsReadyToPlay() {
+    // First wait until every player has selected an avatar
+    await this.gameIsUpdated((game) => {
+      return !game.players.find((player) => !player.avatar);
     });
+
+    // Then wait until all player documents are loaded
+    await Promise.all(this.state.game.players.map((p, i) => this.playerIsUpdated(i, true)));
+  }
+
+  startButtonIsClicked() {
+    return new Promise((resolve, reject) => {
+      this.notifyStartButton = () => {
+        this.notifyStartButton = null;
+        resolve();
+      };
+    });
+  }
+
+  pickNextSubject() {
+    const { players } = this.state.game;
+    const unusedPlayerIndices = players
+      .map((player, index) => ({ doc: player, index }))
+      .filter(({ doc }) => !doc.usedSubject)
+      .map(({ index }) => index);
+    console.log(unusedPlayerIndices);
+
+    const indexIndex = Math.floor(Math.random() * unusedPlayerIndices.length);
+    console.log({ indexIndex });
+    this.setState({ currentSubjectIndex: unusedPlayerIndices[indexIndex] });
+  }
+
+  async showCurrentSubject() {
+    this.setState({
+      gameState: SHOW_CURRENT_SUBJECT,
+      currentSubject: this.state.players[this.state.currentSubjectIndex],
+      currentQuestion: null,
+      currentQuestionIndex: -1
+    });
+    await delay(4000);
+  }
+
+  async showCurrentQuestion(index) {
+    const currentQuestion = this.state.currentSubject.questions[index];
+
+    this.setState({
+      gameState: SHOW_CURRENT_QUESTION,
+      currentQuestionIndex: index,
+      currentQuestion
+    });
+
+    for (let i = 0; i < this.state.players.length; i++) {
+      const player = this.state.players[i];
+      if (i === index) {
+        await this.updateDoc('players', player.id, {
+          currentQuestion: null,
+          isRight: null
+        });
+      } else {
+        await this.updateDoc('players', player.id, {
+          currentQuestion,
+          isRight: null
+        });
+      }
+    }
+  }
+
+  async allPlayersHaveAnswered() {
+    console.log('allPlayersHaveAnswered : starting to wait');
+    await Promise.all(
+      this.state.players.map((player, index) =>
+        this.playerIsUpdated(
+          index,
+          (updatedPlayer) => updatedPlayer.currentQuestion == null || typeof updatedPlayer.isRight === 'boolean'
+        )
+      )
+    );
+    console.log('allPlayersHaveAnswered : waiting done');
+  }
+
+  // -----------------------------------------------
+  // Boilerplate code - this could be moved to a
+  // base class, and hidden away from view!
+
+  componentDidUpdate(oldProps, oldState) {
+    if (!same(oldState.game, this.state.game)) {
+      const newGameUpdateCallbacks = [];
+      this.gameUpdateCallbacks.forEach(({ filter, callback, once }) => {
+        const filterResult = typeof filter === 'function' ? filter(this.state.game) : filter;
+        if (filterResult) {
+          callback(this.state.game);
+        }
+        if (!(filterResult && once)) {
+          newGameUpdateCallbacks.push({ filter, callback, once });
+        }
+      });
+      this.gameUpdateCallbacks = newGameUpdateCallbacks;
+    }
+
+    for (let i = 0; i < oldState.players.length || i < this.state.players.length; i++) {
+      const player = this.state.players[i];
+      if (!same(oldState.players[i], player)) {
+        console.log(
+          `Received update for player with index ${i} (checking ${this.playerUpdateCallbacks.length} filters...)`
+        );
+        const newPlayerUpdateCallbacks = [];
+        this.playerUpdateCallbacks.forEach(({ index, filter, callback, once }) => {
+          const filterResult = index === i && (typeof filter === 'function' ? filter(player, i) : filter);
+
+          console.log(`Matching filter: ${filterResult}`);
+          if (filterResult) {
+            callback(player, i);
+          }
+          if (!(filterResult && once)) {
+            newPlayerUpdateCallbacks.push({ index, filter, callback, once });
+          }
+        });
+        this.playerUpdateCallbacks = newPlayerUpdateCallbacks;
+      }
+    }
   }
 
   whenGameIsUpdated(filter, callback, once) {
     this.gameUpdateCallbacks.push({ filter, callback, once });
   }
 
+  whenPlayerIsUpdated(index, filter, callback, once) {
+    this.playerUpdateCallbacks.push({ index, filter, callback, once });
+  }
+
   gameIsUpdated(filter) {
     return new Promise((resolve, reject) => {
-      if (typeof filter === "function" && this.state.game) {
-        if (filter(this.state.game)) {
-          resolve(this.state.game);
-        }
-      } else if (typeof filter === "function") {
-        this.whenGameIsUpdated(filter, resolve, true);
-      } else if (filter) {
+      if (typeof filter === 'function' && this.state.game && filter(this.state.game)) {
+        resolve(this.state.game);
+      } else if (filter === true && this.state.game) {
         resolve(this.state.game);
       } else {
-        reject();
+        this.whenGameIsUpdated(filter, resolve, true);
       }
     });
   }
 
-  // This method waits until all players have created questions, and selected avatars
-  async gameIsReadyToPlay() {
-    await this.gameIsUpdated(game => {
-      return !game.players.find(player => !player.avatar);
+  playerIsUpdated(index, filter) {
+    return new Promise((resolve, reject) => {
+      if (
+        typeof filter === 'function' &&
+        this.state.players &&
+        this.state.players[index] &&
+        filter(this.state.players[index], index)
+      ) {
+        resolve(this.state.players[index]);
+      } else if (filter === true && this.state.players && this.state.players[index]) {
+        resolve(this.state.players[index]);
+      } else {
+        this.whenPlayerIsUpdated(index, filter, resolve, true);
+      }
     });
-    // TODO: wait until all player docs are loaded
+  }
+
+  render() {
+    const { children } = this.props;
+
+    const childrenWithGameEngine = React.Children
+      .toArray(children)
+      .filter((child) => child.props.gameState === this.state.gameState)
+      .map((child) =>
+        React.cloneElement(child, {
+          gameEngine: this,
+          currentSubject: this.state.currentSubject,
+          currentQuestion: this.state.currentQuestion,
+          scores: this.state.players.map((p) => ({ name: p.name, avatar: p.avatar, score: p.score }))
+        })
+      );
+
+    return <React.Fragment>{childrenWithGameEngine}</React.Fragment>;
   }
 }
 
@@ -130,21 +275,45 @@ export default GameEngine;
 class GameEngineTestChild extends React.Component {
   render() {
     return (
-      <p>
-        {"gameEngine.name = "}
-        {this.props.gameEngine.name}
-      </p>
+      <React.Fragment>
+        <h1>{this.props.text}</h1>
+        {this.props.gameState === 'READY_TO_PLAY' && (
+          <button onClick={() => this.props.gameEngine.notifyStartButton()}>START GAME</button>
+        )}
+        <h2>props[{this.props.propsKey}]</h2>
+        {this.props.propsKey && (
+          <pre>
+            <code>{JSON.stringify(this.props[this.props.propsKey], null, '  ')}</code>
+          </pre>
+        )}
+        <h2>gameEngine.state</h2>
+        <pre>
+          <code>{JSON.stringify(this.props.gameEngine.state, null, '  ')}</code>
+        </pre>
+      </React.Fragment>
     );
   }
 }
 
-const GameEngineTestView = props => {
+const GameEngineTestView = (props) => {
   return (
     <Consumer>
-      {value => {
+      {(value) => {
         return (
           <GameEngine value={value} gameId={props.match.params.gameId}>
-            <GameEngineTestChild />
+            <GameEngineTestChild text="No state" gameState="NONE" />
+            <GameEngineTestChild text="Waiting state" gameState="WAITING" />
+            <GameEngineTestChild text="Ready to play" gameState="READY_TO_PLAY" />
+            <GameEngineTestChild
+              text="Showing current subject"
+              propsKey="currentSubject"
+              gameState="SHOW_CURRENT_SUBJECT"
+            />
+            <GameEngineTestChild
+              text="Showing current question"
+              propsKey="currentQuestion"
+              gameState="SHOW_CURRENT_QUESTION"
+            />
           </GameEngine>
         );
       }}
--- src/context.js
+++ src/context.js
@@ -1,11 +1,10 @@
-import React, { Component } from "react";
-import Firebase from "./Firebase";
-import { strictEqual } from "assert";
+import React, { Component } from 'react';
+import Firebase from './Firebase';
 
 const Context = React.createContext();
 
 const answeredReducer = async (state, isRight) => {
-  const player = await Firebase.saveDoc("players", {
+  const player = await Firebase.saveDoc('players', {
     ...state.player,
     isRight: isRight
   });
@@ -14,29 +13,29 @@ const answeredReducer = async (state, isRight) => {
 
 const updateTitleinDatabase = async (state, title) => {
   // Update the game.title value with the new game title
-  const game = await Firebase.saveDoc("games", { ...state.game, title: title });
+  const game = await Firebase.saveDoc('games', { ...state.game, title: title });
   return { ...state, game };
 };
 
 const addDateinDatabase = async (state, date) => {
   // Update the game.date value with the new date
-  const game = await Firebase.saveDoc("games", { ...state.game, date: date });
+  const game = await Firebase.saveDoc('games', { ...state.game, date: date });
   return { ...state, game };
 };
 
 const addAvatarinDatabase = async (state, avatar) => {
   // Update the game.date value with the new date
-  const player = await Firebase.saveDoc("players", {
+  const player = await Firebase.saveDoc('players', {
     ...state.player,
     avatar: avatar
   });
-  const game = await Firebase.readDoc("games", state.player.gameId);
-  game.players.forEach(player => {
+  const game = await Firebase.readDoc('games', state.player.gameId);
+  game.players.forEach((player) => {
     if (player.id === state.player.id) {
       player.avatar = avatar;
     }
   });
-  await Firebase.saveDoc("games", {
+  await Firebase.saveDoc('games', {
     ...game,
     id: state.player.gameId
   });
@@ -44,29 +43,29 @@ const addAvatarinDatabase = async (state, avatar) => {
 };
 const addQuestioninDatabase = async (state, question) => {
   // Add a new question to the end of the player.questions array
-  const player = await Firebase.saveDoc("players", {
+  const player = await Firebase.saveDoc('players', {
     ...state.player,
-    questions: [...state.player.questions, question]
+    questions: [ ...state.player.questions, question ]
   });
   return { ...state, player };
 };
 
 const deletePlayerinDatabase = async (state, id) => {
   // filter out the memeber you want to delete from the games array
-  const game = await Firebase.saveDoc("games", {
+  const game = await Firebase.saveDoc('games', {
     ...state.game,
-    players: state.game.players.filter(player => player.id !== id)
+    players: state.game.players.filter((player) => player.id !== id)
   });
   //delete the document
-  await Firebase.deleteDoc("players", id);
+  await Firebase.deleteDoc('players', id);
   return { ...state, game };
 };
 
 const addPlayerinDatabase = async (state, name) => {
   // First create a new document in the players collection
-  const newPlayerDoc = await Firebase.saveDoc("players", {
+  const newPlayerDoc = await Firebase.saveDoc('players', {
     name: name,
-    avatar: "",
+    avatar: '',
     gameId: state.game.id,
     questions: []
   });
@@ -75,7 +74,7 @@ const addPlayerinDatabase = async (state, name) => {
   const newPlayerId = newPlayerDoc.id;
 
   // Then update the game state to add the new player and their id to the game.players array
-  const game = await Firebase.saveDoc("games", {
+  const game = await Firebase.saveDoc('games', {
     ...state.game,
     players: [
       ...state.game.players,
@@ -90,7 +89,7 @@ const addPlayerinDatabase = async (state, name) => {
 };
 
 const updateGameDoc = async (game, changes) => {
-  return await Firebase.saveDoc("games", Object.assign({}, game, changes));
+  return await Firebase.saveDoc('games', Object.assign({}, game, changes));
 };
 
 const updateGame = async (state, changes) => {
@@ -99,24 +98,19 @@ const updateGame = async (state, changes) => {
 };
 
 const updatePlayerDoc = async (player, changes) => {
-  return await Firebase.saveDoc("players", Object.assign({}, player, changes));
+  return await Firebase.saveDoc('players', Object.assign({}, player, changes));
 };
 
-const showNextSubject = async state => {
+const showNextSubject = async (state) => {
   const { currentSubjectId } = state.game;
-  const currentPlayerIndex = state.game.players.findIndex(
-    p => p.id === currentSubjectId
-  );
+  const currentPlayerIndex = state.game.players.findIndex((p) => p.id === currentSubjectId);
   const nextPlayerIndex = currentPlayerIndex + 1;
 
   if (nextPlayerIndex >= state.game.players.length) {
     // TODO: Show final game over
     return state;
   } else {
-    return await setCurrentSubjectId(
-      state,
-      state.game.players[nextPlayerIndex].id
-    );
+    return await setCurrentSubjectId(state, state.game.players[nextPlayerIndex].id);
   }
 };
 
@@ -135,15 +129,12 @@ const setCurrentSubjectId = async (state, payload) => {
   });
 };
 
-const showNextQuestion = async state => {
+const showNextQuestion = async (state) => {
   // payload : nothing
-  const currentSubject = state.players.find(
-    p => p.id === state.game.currentSubjectId
-  );
+  const currentSubject = state.players.find((p) => p.id === state.game.currentSubjectId);
 
   const currentQuestionIndex = state.game.currentQuestionIndex;
-  const nextQuestionIndex =
-    currentQuestionIndex >= -1 ? currentQuestionIndex + 1 : 0;
+  const nextQuestionIndex = currentQuestionIndex >= -1 ? currentQuestionIndex + 1 : 0;
 
   if (nextQuestionIndex >= currentSubject.questions.length) {
     return await showNextSubject(state);
@@ -154,9 +145,7 @@ const showNextQuestion = async state => {
 
 const setCurrentQuestionIndex = async (state, payload) => {
   // payload : index of question
-  const currentSubject = state.players.find(
-    p => p.id === state.game.currentSubjectId
-  );
+  const currentSubject = state.players.find((p) => p.id === state.game.currentSubjectId);
   const currentQuestion = currentSubject.questions[payload];
   const shownQuestion = {
     title: `Question ${payload + 1} of ${currentSubject.questions.length}`,
@@ -178,10 +167,10 @@ const setCurrentQuestionIndex = async (state, payload) => {
   });
 };
 
-const updateQuestionScores = async state => {
+const updateQuestionScores = async (state) => {
   // payload: nothing
-  const newGamePlayers = state.game.players.map(player => {
-    const fullPlayer = state.players.find(fp => fp.id === player.id);
+  const newGamePlayers = state.game.players.map((player) => {
+    const fullPlayer = state.players.find((fp) => fp.id === player.id);
     if (fullPlayer && fullPlayer.isRight) {
       // TODO: Increase score percent by (100 % / number of questions about subject)
       return { ...player, score: (player.score || 0) + 20 };
@@ -203,42 +192,42 @@ const updateQuestionScores = async state => {
 const reducer = async (state, action) => {
   //evaluate the action
   switch (action.type) {
-    case "NEW_QUIZ":
+    case 'NEW_QUIZ':
       return {
         ...state
         //start a new set of questions for a new tem member
       };
-    case "UPDATE_GAMETITLE":
+    case 'UPDATE_GAMETITLE':
       //update database with the title of the game
       return await updateTitleinDatabase(state, action.payload);
-    case "UPDATE_GAMEDATE":
+    case 'UPDATE_GAMEDATE':
       //update database with the date
       return await addDateinDatabase(state, action.payload);
-    case "ADD_PLAYER":
+    case 'ADD_PLAYER':
       //update database with the players name
       return await addPlayerinDatabase(state, action.payload);
-    case "ADD_QUESTION":
+    case 'ADD_QUESTION':
       //update database, add a question to the player document
       return await addQuestioninDatabase(state, action.payload);
-    case "ADD_AVATAR":
+    case 'ADD_AVATAR':
       //add the avatar in the player doc
       return await addAvatarinDatabase(state, action.payload);
-    case "ANSWERED":
+    case 'ANSWERED':
       //send the player id and isRight answer to the database to uppdate the scoreboard
       return await answeredReducer(state, action.payload);
-    case "DELETE_MEMBER":
+    case 'DELETE_MEMBER':
       return await deletePlayerinDatabase(state, action.payload);
-    case "SET_CURRENT_SUBJECT_ID":
+    case 'SET_CURRENT_SUBJECT_ID':
       return await setCurrentSubjectId(state, action.payload);
-    case "SET_CURRENT_QUESTION_INDEX":
+    case 'SET_CURRENT_QUESTION_INDEX':
       return await setCurrentQuestionIndex(state, action.payload);
-    case "UPDATE_QUESTION_SCORES":
+    case 'UPDATE_QUESTION_SCORES':
       return await updateQuestionScores(state);
-    case "SHOW_NEXT_QUESTION":
+    case 'SHOW_NEXT_QUESTION':
       return await showNextQuestion(state);
 
     default:
-      console.error("unknown action", action);
+      console.error('unknown action', action);
       return state;
   }
 };
@@ -250,89 +239,80 @@ export class Provider extends Component {
 
     player: {},
 
-    dispatch: async action => {
+    dispatch: async (action) => {
       const newState = await reducer(this.state, action);
       this.setState(newState);
     },
 
     subscribeDoc: (collection, id, callback) => {
-      Firebase.firestore()
-        .collection(collection)
-        .doc(id)
-        .onSnapshot(doc => {
-          const docData = {
-            ...doc.data(),
-            id
-          };
-          callback(docData);
-        });
+      Firebase.firestore().collection(collection).doc(id).onSnapshot((doc) => {
+        const docData = {
+          ...doc.data(),
+          id
+        };
+        callback(docData);
+      });
+    },
+
+    updateDoc: async (collection, id, changes) => {
+      const doc = await Firebase.readDoc(collection, id);
+      await Firebase.saveDoc(collection, Object.assign({}, doc, changes));
     },
 
     //subscribe a game
-    subscribe: gameId => {
+    subscribe: (gameId) => {
       // Om det redan finns en subscription, avbryt den.
       if (this.unsubscribeGame) this.unsubscribeGame();
 
       // Börja hämta ett dokument i collection "games" med det id
       // som skickats.
-      this.unsubscribeGame = Firebase.firestore()
-        .collection("games")
-        .doc(gameId)
-        .onSnapshot(doc => {
-          // När det kommer en uppdatering, läs ut dokumentet
-          const game = {
-            ...doc.data(),
-            id: gameId
-          };
+      this.unsubscribeGame = Firebase.firestore().collection('games').doc(gameId).onSnapshot((doc) => {
+        // När det kommer en uppdatering, läs ut dokumentet
+        const game = {
+          ...doc.data(),
+          id: gameId
+        };
 
-          // ... och uppdatera state, så att vyn renderas om automatiskt.
-          this.setState({ game: game });
-        });
+        // ... och uppdatera state, så att vyn renderas om automatiskt.
+        this.setState({ game: game });
+      });
     },
 
     //subscribe a player
-    subscribePlayer: playerId => {
+    subscribePlayer: (playerId) => {
       if (this.unsubscribePlayer) this.unsubscribePlayer();
 
-      this.unsubscribePlayer = Firebase.firestore()
-        .collection("players")
-        .doc(playerId)
-        .onSnapshot(doc => {
-          const player = {
-            ...doc.data(),
-            id: playerId
-          };
-          this.setState({ player: player });
-        });
+      this.unsubscribePlayer = Firebase.firestore().collection('players').doc(playerId).onSnapshot((doc) => {
+        const player = {
+          ...doc.data(),
+          id: playerId
+        };
+        this.setState({ player: player });
+      });
     },
 
     // subscribe multiple players
-    subscribePlayers: playerIds => {
+    subscribePlayers: (playerIds) => {
       if (this.unsubscribePlayers) {
-        this.unsubscribePlayers.forEach(unsubscribeCallback =>
-          unsubscribeCallback()
-        );
+        this.unsubscribePlayers.forEach((unsubscribeCallback) => unsubscribeCallback());
       }
 
       const tempPlayers = [];
       let playersCount = 0;
 
       this.unsubscribePlayers = playerIds.map((playerId, index) => {
-        const unsubscribe = Firebase.firestore()
-          .collection("players")
-          .doc(playerId)
-          .onSnapshot(doc => {
-            const player = {
-              ...doc.data(),
-              id: playerId
-            };
+        const unsubscribe = Firebase.firestore().collection('players').doc(playerId).onSnapshot((doc) => {
+          const player = {
+            ...doc.data(),
+            id: playerId
+          };
 
-            tempPlayers[index] = player;
-            ++playersCount;
-            if (playersCount >= playerIds.length) {
-              this.setState({ players: tempPlayers });
-            }
-          });
+          tempPlayers[index] = player;
+          ++playersCount;
+          if (playersCount >= playerIds.length) {
+            this.setState({ players: tempPlayers });
+          }
+        });
         return unsubscribe;
       });
     }
@@ -342,9 +322,7 @@ export class Provider extends Component {
     //the provider provides the value
     return (
       //we pass the entire state
-      <Context.Provider value={this.state}>
-        {this.props.children}
-      </Context.Provider>
+      <Context.Provider value={this.state}>{this.props.children}</Context.Provider>
     );
   }
 }
