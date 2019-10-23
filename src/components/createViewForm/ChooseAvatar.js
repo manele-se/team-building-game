222017b797812f9677236e34f1104f5cfe50f17b
Elena <elena@manele.se>
2019-10-23 20:37:49 +0200

    Fixed broken things after refactoring

--- src/components/createViewForm/ChooseAvatar.js
+++ src/components/createViewForm/ChooseAvatar.js
@@ -4,52 +4,62 @@ import './choose-avatar.css';
 import '../scoreView/score.css';
 import Header from '../../layouts/Header';
 import { Consumer } from '../../context';
+import Spinner from '../../layouts/Spinner';
 
 //save the avatar which correspond to the playerId in the database
 class ChooseAvatar extends React.Component {
-	state = {
-		selectedAvatar: ''
-	};
-	onAvatarSelected = (dispatch, avatarName) => {
-		this.setState({
-			selectedAvatar: avatarName
-		});
-		dispatch({
-			type: 'ADD_AVATAR',
-			payload: avatarName
-		});
-	};
+  state = {
+    selectedAvatar: ''
+  };
+  onAvatarSelected = (dispatch, avatarName) => {
+    this.setState({
+      selectedAvatar: avatarName
+    });
+    dispatch({
+      type: 'ADD_AVATAR',
+      payload: avatarName
+    });
+  };
 
-	render() {
-		return (
-			<Consumer>
-				{(value) => {
-					const allAvatarNames = Object.getOwnPropertyNames(IMG);
-					const { playerId } = this.props.match.params;
-					const { dispatch } = value;
+  render() {
+    return (
+      <Consumer>
+        {(value) => {
+          const allAvatarNames = Object.getOwnPropertyNames(IMG);
+          const { playerId } = this.props.match.params;
+          const { dispatch, player } = value;
 
-					return (
-						<React.Fragment>
-							<Header canContinue={this.state.selectedAvatar} continueUrl={`/player/ready/${playerId}`} />
-							<h1 className="titelAvatar">Choose your avatar!</h1>
-							<div className="container chooseAvatar">
-								{allAvatarNames.map((avatarName) => (
-									<img
-										className={this.state.selectedAvatar === avatarName ? 'selected' : ''}
-										src={IMG[avatarName]}
-										alt="img"
-										onClick={() => {
-											this.onAvatarSelected(dispatch, avatarName);
-										}}
-									/>
-								))}
-							</div>
-						</React.Fragment>
-					);
-				}}
-			</Consumer>
-		);
-	}
+          if (player && player.id) {
+            return (
+              <React.Fragment>
+                <Header canContinue={this.state.selectedAvatar} continueUrl={`/player/ready/${playerId}`} />
+                <h1 className="titelAvatar">Choose your avatar!</h1>
+                <div className="container chooseAvatar">
+                  {allAvatarNames.map((avatarName) => (
+                    <img
+                      className={this.state.selectedAvatar === avatarName ? 'selected' : ''}
+                      src={IMG[avatarName]}
+                      alt="img"
+                      onClick={() => {
+                        this.onAvatarSelected(dispatch, avatarName);
+                      }}
+                    />
+                  ))}
+                </div>
+              </React.Fragment>
+            );
+          } else {
+            value.subscribePlayer(playerId);
+            return (
+              <div>
+                <Spinner />
+              </div>
+            );
+          }
+        }}
+      </Consumer>
+    );
+  }
 }
 
 export default ChooseAvatar;
--- src/components/playerView/TeamView.js
+++ src/components/playerView/TeamView.js
@@ -5,37 +5,37 @@ import soundfile from '../../music/music4.mp3';
 import Spinner from '../../layouts/Spinner';
 
 const TeamView = (props) => {
-	const { gameId } = props.match.params;
-	return (
-		<Consumer>
-			{(value) => {
-				const { game } = value;
-				if (game && game.id) {
-					return (
-						<div className="customContainer avatars">
-							<div className="container d-flex flex-column justify-content-center align-items-center">
-								<h1 className="titleWelcome">The Team</h1>
-								{game.players.map((player) => (
-									<div className="container d-flex flex-row justify-content-center align-items-center">
-										<img src={avatars[player.avatar]} alt="avatar" />
-										<h3>{player.name}</h3>
-									</div>
-								))}
-							</div>
-							<audio src={soundfile} autoPlay loop />
-						</div>
-					);
-				} else {
-					value.subscribe(gameId);
-					return (
-						<div>
-							<Spinner />
-						</div>
-					);
-				}
-			}}
-		</Consumer>
-	);
+  const { gameId } = props.match.params;
+  return (
+    <Consumer>
+      {(value) => {
+        const { game } = value;
+        if (game && game.id) {
+          return (
+            <div className="customContainer avatars">
+              <div className="container d-flex flex-column justify-content-center align-items-center">
+                <h1 className="titleWelcome">The Team</h1>
+                {game.players.map((player) => (
+                  <div className="container d-flex flex-row justify-content-center align-items-center">
+                    <img src={avatars[player.avatar]} alt="avatar" />
+                    <h3>{player.name}</h3>
+                  </div>
+                ))}
+              </div>
+              <audio src={soundfile} autoPlay loop />
+            </div>
+          );
+        } else {
+          value.subscribeGame(gameId);
+          return (
+            <div>
+              <Spinner />
+            </div>
+          );
+        }
+      }}
+    </Consumer>
+  );
 };
 
 export default TeamView;
